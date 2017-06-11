922e603a56755c4ecca54d7e4307613349352078
abdo20 <abdelrhmanadel93@gmail.com>
2017-06-11 09:05:12 +0200

    added user signup test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   	def user_params
-  		params.require(:usr).permit(:name, :email, :password, :password_confirmation)
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
 end
