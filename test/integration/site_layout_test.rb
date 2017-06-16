80c1d131baf1e12cfe4a9c83bf2df5e1987c74df
abdo20 <abdelrhmanadel93@gmail.com>
2017-06-16 21:23:59 +0200

    added layout links test for login user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links for visitor" do
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path, count: 2
@@ -13,5 +17,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select "a[href=?]", about_path
   	assert_select "a[href=?]", contact_path
   	assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links for logged-in user" do
+    log_in_as @user
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
