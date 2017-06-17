d06896ffe750dc73b586f06d7decdaf79dddca82
abdo20 <abdelrhmanadel93@gmail.com>
2017-06-17 13:52:02 +0200

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -17,14 +17,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   	assert_select 'div.pagination', 2
   	first_page_of_users = User.paginate(page: 1)
   	first_page_of_users.each do |user|
-	  assert_select 'a[href=?]', user_path(user), text: user.name
-	  unless user == @admin
+	   assert_select 'a[href=?]', user_path(user), text: user.name
+	   unless user == @admin
 	  	assert_select 'a[href=?]', user_path(user), text: 'delete'
+	   end
 	  end
-	end
-	assert_difference 'User.count', -1 do
-		delete user_path(@non_admin)
-	end
+	  assert_difference 'User.count', -1 do
+		  delete user_path(@non_admin)
+	  end
   end
 
   test "index as non-admin" do
