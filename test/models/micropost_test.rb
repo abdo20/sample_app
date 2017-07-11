2762e15b47a3ef4891c542007db4975babe3b351
abdo20 <abdelrhmanadel93@gmail.com>
2017-07-11 23:53:42 +0200

    added micropost model test for validation

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -5,5 +5,28 @@ class MicropostTest < ActiveSupport::TestCase
   #   assert true
   # end
 
+  def setup
+    @user = users(:michael)
+    @micropost = @user.Microposts.build(content: "hello world!")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user_id should be present" do
+    @micropost.user_id=nil
+    assert_not @micropost.valid?
+  end
+
+  test "content shold be present" do
+    @micropost.content = "    "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be less than 140 character" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
   
 end
